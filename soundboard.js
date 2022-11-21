6d1edd495cd80cb3b130fa1aeaeacf88bf82bda0
Mithilya <113309804+Mithilya@users.noreply.github.com>
2022-11-20 23:43:17 -0500

    soundboard

---
+++ soundboard.js
@@ -0,0 +1,29 @@
+ 
+function playbell() {
+   
+    var chinabell = new Audio ("Chinabell.wav"); 
+    chinabell.play(); 
+    
+}
+
+ 
+function playkick() {
+   
+    var kick = new Audio ("Kick.wav"); 
+    kick.play(); 
+    
+}
+
+ 
+function playtom() {
+   
+    var floortom = new Audio ("Floortom.wav"); 
+    floortom.play(); 
+    
+}
+
+
+    
+
+
+
